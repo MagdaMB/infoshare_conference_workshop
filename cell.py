c2d85a12456fa04b35bd5ac3d0f7091a8864a593
Przemyslaw Lalak <lalak@protonmail.com>
2018-05-21 15:34:44 +0200

    fix typo

--- cell.py
+++ cell.py
@@ -1,6 +1,3 @@
-import logging
-
-
 class Cell:
     def __init__(self, x, y):
         self.alive = False
@@ -8,19 +5,22 @@ class Cell:
         self.x = x
         self.y = y
 
+    def toggle_status(self):
+        self.alive = not self.alive
+
     def count_alive_neigbhours(self, cells):
         """
         Counts alive neighbours.
         :param cells: list of cells available in grid
         :return: int
         """
-        #@ TODO: add variable that will represent amount of live neighbours
+        # @ TODO: add variable that will represent amount of live neighbours
         # check Moore's neighbourhood (8 neighbours)
         # which are the cells that are horizontally,
         # vertically or diagonally adjacent
-        #@ TODO: loop over neighbourhood
+        # @ TODO: loop over neighbourhood
         # don't check ourself
-        #@ TODO: check the state of a neighbour - if it's alive add it
+        # @ TODO: check the state of a neighbour - if it's alive add it
         pass
 
     def apply_conway_rules(self, cells):
@@ -29,5 +29,5 @@ class Cell:
         :param cells: list of cells available in grid
         :return: True or False (cell lives or dies)
         """
-        #@ TODO: check Conway's rules
+        # @ TODO: check Conway's rules
         pass
